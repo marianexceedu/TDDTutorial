82ca2b54626490db58583f3fc86ba7724d496389
marianExceEdu <marianexceedu@gmail.com>
2020-09-10 14:43:29 +0300

    initial commit.

---
+++ Chatbot/Chatbot.Tests/GreetingTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chatbot.Tests
+{
+    [TestFixture]
+    public class GreetingTests
+    {
+
+        [Test]
+        public void ShouldGreetNormalNames()
+        {
+        }
+    }
+}
